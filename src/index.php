c8951d02018ba17d57a0bb395474f4f911fc3b63
Matthieu-Furvent <ze.karel@free.fr>
2019-03-14 16:47:14 +0100

    Other commit

--- src/index.php
+++ src/index.php
@@ -11,7 +11,7 @@ use App\DataBase\UtilisateurDBUtil;
 //session_start();
 //$user = new Utilisateur("rico", 987, "mdpcool");
 //UtilisateurDBUtil::addUtilisateur($user);
-var_dump(UtilisateurDBUtil::getUtilisateurWithLogin("bob"));
+//var_dump(UtilisateurDBUtil::getUtilisateurWithLogin("bob"));
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,24 +19,24 @@ var_dump(UtilisateurDBUtil::getUtilisateurWithLogin("bob"));
 
 <h2>Inscription</h2>
 
-<form action="/inscription.php" method="post">
+<form action="inscription.php" method="post">
     Login (adresse mail):<br>
-  <input type="text" name="login" value="Entrez votre email">
+  <input type="text" name="loginCreation">
   <br>
     Mot de passe:<br>
-  <input type="text" name="lastname" value="Choisissez un mdp">
+  <input type="text" name="passwordCreation">
   <br><br>
   <input type="submit" value="Submit">
 </form>
 
 <h2>Connexion</h2>
 
-<form action="/connexion.php" method="post">
+<form action="php_tp_we_transfer/src/connexion.php" method="post">
     Login (adresse mail):<br>
     <input type="text" name="login" value="Votre login(email)">
     <br>
     Mot de passe:<br>
-    <input type="text" name="lastname" value="Votre mdp">
+    <input type="text" name="password" value="Votre mdp">
     <br><br>
     <input type="submit" value="Submit">
 </form>
--- src/inscription.php
+++ src/inscription.php
@@ -5,3 +5,31 @@
  * Date: 14/03/2019
  * Time: 14:40
  */
+
+include "../vendor/autoload.php";
+use App\Utilisateur;
+use App\DataBase\UtilisateurDBUtil;
+
+if (!empty($_POST)) {
+    // First form
+    if (isset($_POST['loginCreation']) && isset($_POST['passwordCreation'])) {
+        if ($_POST['loginCreation'] != "" && $_POST['passwordCreation'] != "") {
+            $newUser = new Utilisateur($_POST['loginCreation'], 1000, $_POST['passwordCreation']);
+            UtilisateurDBUtil::addUtilisateur($newUser);
+            echo "Vous êtes bien inscrits";
+            $str = <<<EOD
+            <form action="index.php">
+                <input type="submit" value="Revenir à la page de connexion" />
+            </form>
+EOD;
+            echo $str;
+        } else {
+            header('Location: index.php');
+            exit();
+        }
+    }
+    else {
+        header('Location: index.php');
+        exit();
+    }
+}
